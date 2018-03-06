c00f7f2b065dd98dd8b4bb57db2fb0fff4fd5496
YAWAL <gajkaGAJKA140889>
2018-03-06 10:27:30 +0200

    cobra CLI

---
+++ cmd/root.go
@@ -0,0 +1,21 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"log"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "client",
+	Short: "client functionality",
+	Long:  `client functionality for GetMeConf service`,
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatalf("Error during execution client: %v", err)
+	}
+}
