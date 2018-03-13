25256cb2af0b2384044a7cb525f331c7734f33d1
YAWAL <gajkaGAJKA140889>
2018-03-13 14:12:08 +0200

    editing travis.yml

--- cmd/create_test.go
+++ cmd/create_test.go
@@ -1,42 +1,42 @@
 package cmd
-//
-//import (
-//	"reflect"
-//	"testing"
-//	"encoding/json"
-//	"github.com/YAWAL/GetMeConf/entities"
-//)
-//
-//const (
-//	mongo      = "mongo.csv"
-//	tempConfig = "tempcnf.csv"
-//	tsConfig   = "tscnf.csv"
-//)
-//
-//var mongoWantStruct = entities.Mongodb{Domain: "mongotest", Mongodb: true, Host: "mongo_test", Port: "mongoTest"}
-//var tempWantStruct = entities.Tempconfig{RestApiRoot: "tempconfigtest", Host: "tempconfig_test", Port: "tempConfigTest",
-//	Remoting: "tempconfigtest", LegasyExplorer: false}
-//var tsWantStruct = entities.Tsconfig{Module: "tscnftest", Target: "tscnf_test", SourceMap: false, Excluding: 11}
-//
-//func Test_createByteConfig(t *testing.T) {
-//	mongoWant, _ := json.Marshal(mongoWantStruct)
-//	tempWant, _ := json.Marshal(tempWantStruct)
-//	tsWant, _ := json.Marshal(tsWantStruct)
-//
-//	tests := []struct {
-//		name     string
-//		fileName string
-//		want     []byte
-//	}{
-//		{"mongo", mongo, mongoWant},
-//		{"temp", tempConfig, tempWant},
-//		{"ts", tsConfig, tsWant},
-//	}
-//	for _, tt := range tests {
-//		t.Run(tt.name, func(t *testing.T) {
-//			if got := createByteConfig(tt.fileName); !reflect.DeepEqual(got, tt.want) {
-//				t.Errorf("Test %v: createByteConfig() = %v, want %v", tt.name, got, tt.want)
-//			}
-//		})
-//	}
-//}
+
+import (
+	"reflect"
+	"testing"
+	"encoding/json"
+	"github.com/YAWAL/GetMeConf/entities"
+)
+
+const (
+	mongo      = "mongo.csv"
+	tempConfig = "tempcnf.csv"
+	tsConfig   = "tscnf.csv"
+)
+
+var mongoWantStruct = entities.Mongodb{Domain: "mongotest", Mongodb: true, Host: "mongo_test", Port: "mongoTest"}
+var tempWantStruct = entities.Tempconfig{RestApiRoot: "tempconfigtest", Host: "tempconfig_test", Port: "tempConfigTest",
+	Remoting: "tempconfigtest", LegasyExplorer: false}
+var tsWantStruct = entities.Tsconfig{Module: "tscnftest", Target: "tscnf_test", SourceMap: false, Excluding: 11}
+
+func Test_createByteConfig(t *testing.T) {
+	mongoWant, _ := json.Marshal(mongoWantStruct)
+	tempWant, _ := json.Marshal(tempWantStruct)
+	tsWant, _ := json.Marshal(tsWantStruct)
+
+	tests := []struct {
+		name     string
+		fileName string
+		want     []byte
+	}{
+		{"mongo", mongo, mongoWant},
+		{"temp", tempConfig, tempWant},
+		{"ts", tsConfig, tsWant},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := createByteConfig(tt.fileName); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Test %v: createByteConfig() = %v, want %v", tt.name, got, tt.want)
+			}
+		})
+	}
+}
